7c865e88546337e544381645a6ebb4889d82810c
gaoyuqing18 <gyq0808g@163.com>
2019-08-06 22:06:31 +0800

    老师学生首页完成

--- src/pages/home/teacher/views/TeacherOnline/TeacherOnlineContainer.jsx
+++ src/pages/home/teacher/views/TeacherOnline/TeacherOnlineContainer.jsx
@@ -1,58 +1,41 @@
 import React, { Component } from 'react'
-import 'antd/dist/antd.css';
-import { Switch, Slider,  Row, Col } from 'antd';
-import { Online } from './StyledTeacherOnline'
+import TeacherOnlineUI from './TeacherOnlineUI'
 class TeacherOnlineContainer extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            inputValue: 1,
+            timeValue: 1,
+            rangeValue: 1,
+            priceValue: 1,
         };
+        this.onChangetime = this.onChangetime.bind(this);
+        this.onChangerange = this.onChangerange.bind(this);
+        this.onChangeprice = this.onChangeprice.bind(this);
     }
-    onChange = value => {
+    onChangetime = value => {
         this.setState({
-            inputValue: value,
+            timeValue: value,
+        });
+    };
+    onChangerange = value => {
+        this.setState({
+            rangeValue: value,
+        });
+    };
+    onChangeprice = value => {
+        this.setState({
+            priceValue: value,
         });
     };
     render() {
         return (
-            < Online>
-                <div className="online"><span>是否开启老师上线功能</span> <Switch size="small" defaultChecked className="switch-button" /></div>
-                <div className="sliders">
-                    <div className="time-sliders">
-                        <div className="time-sliders-tip">
-                            <span>时间</span>
-                            <div
-                            min={1}
-                            max={20}
-                            style={{ marginLeft: 16 }}
-                            value={this.state.inputValue}
-                            onChange={this.onChange}
-                            >
-                    {this.state.inputValue}
-                    {
-                      this.state.inputValue < 12 ? ":00 AM": ":00 PM"
-                    }
-                             </div>
-                         </div>
-                       
-                        <Row> 
-                            
-                            <Col span={24}>
-                            <img className="time-src1" src="images/times@3x.png" alt=""></img>
-                                <Slider
-                                    min={0}
-                                    max={23}
-                                    onChange={this.onChange}
-                                    value={typeof this.state.inputValue === 'number' ? this.state.inputValue : 0}
-                                />
-                             <img className="time-src2" src="images/times@3x.png" alt=""></img>
-                            </Col>
-                            
-                        </Row>
-                    </div>
-                </div>
-            </Online>
+            <TeacherOnlineUI 
+            {...this.state}
+            onChangetime = { this.onChangetime}
+            onChangerange= {this.onChangerange}
+            onChangeprice= {this.onChangeprice}
+            >
+            </TeacherOnlineUI>
         )
     }
 }
