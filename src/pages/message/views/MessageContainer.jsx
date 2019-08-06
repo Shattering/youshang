5d4557c19ed8ac26bc59c515ceecea3fb5e4e067
郑军基 <1210605579@qq.com>
2019-08-06 16:47:44 +0800

    本次提交聊天静态页面和订单相关的静态页面zhengjunji

--- src/pages/message/views/MessageContainer.jsx
+++ src/pages/message/views/MessageContainer.jsx
@@ -1,10 +1,13 @@
 import React,{Component} from 'react'
 import MessageUI from './MessageUI'
+import OrderDetailsUI from '../order/OrderDetailsUI'
+import OrderCenterUI from '../order/OrderCenterUI'
 
 class MessageContainer extends Component {
   render() {
     return (
       <MessageUI></MessageUI>
+      // <OrderCenterUI></OrderCenterUI>
     )
   }
 }
